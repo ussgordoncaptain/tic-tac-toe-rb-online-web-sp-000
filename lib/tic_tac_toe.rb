6b6176a6603072dd6e21977295cc21711b16ad46
Edmund Nelson <edmund.nelson.b@gmail.com>
2020-05-07 13:19:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,7 +36,8 @@ end
 def turn(board)
   
 while(true)
- puts(" please make a move 1-9")
+player=current_player[board]
+ puts(" please make a move 1-9 #{player}")
   move_selected= gets.chomp 
   move_index = input_to_index(move_selected) 
   if (valid_move?(board, move_index)== false)
